df3e677ee499762ea43c217594ac8cbe38d6e944
Sandra G <sandyjtech@gmail.com>
2023-06-14 10:21:48 -0700

    added pseudocode

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  reverseWord = word.split("").reverse().join("");
+  if (word === reverseWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
+isPalindrome("abba");
+isPalindrome("racecar");
+isPalindrome("a");
+isPalindrome("robot");
+isPalindrome("ab");
 /* 
   Add your pseudocode here
 */
-
+console.log("Expecting: true");
 /*
   Add written explanation of your solution here
+  If our word is same as our reverse word, then it is a palindrome
 */
 
-// You can run `node index.js` to view these console logs
+// // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
